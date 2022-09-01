c681b3a4d4fbd8c223dca159407ce6673ec576ea
ayxan <ayxan.shahsuvarov1@gmail.com>
2022-09-01 07:19:14 +0400

    STATIC_ROOT path fixed

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -14,8 +14,7 @@ DATABASES = {
 
 STATIC_URL = '/static/'
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
