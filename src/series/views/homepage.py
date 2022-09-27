221c7536906c52efb5c0cd30aaf7996e1225396d
ayxan <ayxan.shahsuvarov1@gmail.com>
2022-09-27 22:17:30 +0400

    fixed series list

--- src/series/views/homepage.py
+++ src/series/views/homepage.py
@@ -6,8 +6,10 @@ from account.models import User
 
 @login_required(login_url='/account/login')
 def homepage_view(request):
-    series = User.objects.get(id=request.user.id).series.all().order_by('-id')
+    user = User.objects.get(id=request.user.id)
+    series_true = user.series.filter(show=True).order_by('-id')
+    series_false = user.series.filter(show=False).order_by('-id')
     page = request.GET.get('page')
-    paginator = Paginator(series, 5)
+    paginator = Paginator(list(series_true) + list(series_false), 5)
 
     return render(request, 'homepage.html', context={'series': paginator.get_page(page)})
