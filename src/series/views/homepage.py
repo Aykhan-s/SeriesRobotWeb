0ad9f71f2a897395ec5f45d6516e30778747e7d8
ayxan <ayxan.shahsuvarov1@gmail.com>
2022-12-20 17:15:43 +0400

    Added filter with radio buttons

--- src/series/views/homepage.py
+++ src/series/views/homepage.py
@@ -9,17 +9,28 @@ from django.db.models import Q
 def homepage_view(request):
     user = User.objects.get(id=request.user.id)
 
-    if request.GET.get('new') == 'true':
+    type_ = request.GET.get('type')
+    if type_ == 'unwatched':
         series = user.series.filter(~Q(new_episodes_count=0)).order_by('-id')
+
+    elif type_ == 'watched':
+        series = user.series.filter(new_episodes_count=0).order_by('-id')
 
     else: series = user.series.filter().order_by('-id')
 
-    if search := request.GET.get('search'):
+    search = request.GET.get('search', 'None')
+    if search != 'None':
         series = series.filter(
             Q(title__icontains=search)
         )
 
+    def kwargs():
+        queries = request.GET.copy()
+        queries.pop('page', 0)
+        return queries.urlencode()
+
     page = request.GET.get('page')
     paginator = Paginator(series, 5)
 
-    return render(request, 'homepage.html', context={'series': paginator.get_page(page)})
+    return render(request, 'homepage.html', context={'series': paginator.get_page(page),
+                                                    'kwargs': kwargs()})
