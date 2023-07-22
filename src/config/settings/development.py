64272c99aa8e86daf036d23b28a80448a186c605
Aykhan <aykhan.shahs0@gmail.com>
2023-07-22 17:22:27 +0400

    python version 3.10-alpine --> 3.10-bullseye

--- src/config/settings/development.py
+++ src/config/settings/development.py
@@ -9,8 +9,12 @@ CSRF_TRUSTED_ORIGINS=['http://127.0.0.1:8000']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
